fb3416a09bc5cc04cd2470056dc5351a7b859087
choraporbacon-debug <choraporbacon@gmail.com>
2026-09-10 20:42:29 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Renomeie este arquivo para config.js e coloque os dados do seu projeto.
 // NUNCA use a chave service_role no navegador.
-window.NC_SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-window.NC_SUPABASE_KEY = "SUA_CHAVE_PUBLISHABLE_OU_ANON";
+window.NC_SUPABASE_URL = "https://hbgpvdefqsilxgcakdns.supabase.co";
+window.NC_SUPABASE_KEY = "sb_publishable_U6YQovRGQxKqZkIwl8g6CQ_UoIlL9Sl";
